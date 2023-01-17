cc4bef1cc6dac4ab1a34ede81dcd1945cabed296
Richard Lee <14349+dlackty@users.noreply.github.com>
2023-01-18 00:13:56 +0800

    Add support for collection caching

--- lib/cache_with_locale.rb
+++ lib/cache_with_locale.rb
@@ -1,16 +1,1 @@
 require "cache_with_locale/railtie"
-
-module CacheWithLocale
-  module Helpers
-    def cache(key, options = {}, &block)
-      super(cache_with_locale_compose_key(key), options) do
-        yield(block)
-      end
-    end
-
-    private
-    def cache_with_locale_compose_key(key)
-      Array.wrap(key) + [I18n.locale.to_s]
-    end
-  end
-end
--- lib/cache_with_locale/railtie.rb
+++ lib/cache_with_locale/railtie.rb
@@ -1,8 +1,12 @@
+require "cache_with_locale/collection_caching"
+require "cache_with_locale/helpers"
+
 module CacheWithLocale
   class Railtie < ::Rails::Railtie
     initializer 'rails_db.helpers' do
       ActiveSupport.on_load :action_view do
         ActionView::Base.send :include, CacheWithLocale::Helpers
+        ActionView::PartialRenderer.prepend CacheWithLocale::CollectionCaching
       end
     end
   end
